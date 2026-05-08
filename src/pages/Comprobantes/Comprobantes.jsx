c157cd0fb82edafaf9f0c2351661714959afe046
Luis Cajacuri <luiscajacurigutierrezgmail.com>
2026-05-08 10:06:03 -0500

    feat: creación de las páginas principales del sistema

---
+++ src/pages/Comprobantes/Comprobantes.jsx
@@ -0,0 +1,7 @@
+export default function Comprobantes(){
+  return(
+    <div>
+      <h1>Comprobantes</h1>
+    </div>
+  )
+}
---
+++ src/pages/NuevaVenta/NuevaVenta.jsx
@@ -0,0 +1,7 @@
+export default function NuevaVenta(){
+  return(
+    <div>
+      <h1>Nueva Venta</h1>
+    </div>
+  )
+}
